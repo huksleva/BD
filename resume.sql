7ef3f4388024d92e5d59dccd5c2cb439503359ca
Leonid Tots <59145792+huksleva@users.noreply.github.com>
2026-02-13 15:14:02 +0300

    123

---
+++ resume.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM simpledb.resume
+LIMIT 0, 1000
+
+-- Date: 2026-02-05 18:02
+*/
+INSERT INTO `` (`resumeid`,`userid`,`title`,`skills`,`created`) VALUES (5,2,'doklad1','clairvoyance','2026-02-05 17:58:55');
+INSERT INTO `` (`resumeid`,`userid`,`title`,`skills`,`created`) VALUES (6,3,'doklad2','magic','2026-02-05 17:58:55');
+INSERT INTO `` (`resumeid`,`userid`,`title`,`skills`,`created`) VALUES (7,1,'doklad3','X-RAY','2026-02-05 17:58:55');
+INSERT INTO `` (`resumeid`,`userid`,`title`,`skills`,`created`) VALUES (8,4,'doklad4','Invisibility','2026-02-05 17:58:55');
---
+++ users.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM simpledb.users
+LIMIT 0, 1000
+
+-- Date: 2026-02-05 17:29
+*/
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,`created`) VALUES (1,'Ivan','ivan@gmail.com',NULL,NULL,NULL,NULL,'2026-02-05 17:20:54');
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,`created`) VALUES (2,'Alex','alex@gmail.com',NULL,NULL,NULL,NULL,'2026-02-05 17:20:54');
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,`created`) VALUES (3,'Leo','leo@gmail.com',NULL,NULL,NULL,NULL,'2026-02-05 17:20:54');
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,`created`) VALUES (4,'Vova','vova@gmail.com',NULL,NULL,NULL,NULL,'2026-02-05 17:24:51');
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,`created`) VALUES (5,'Ekaterina','ekaterina.petrova@outlook.com','F','2000-02-11','145789',1.123,'2026-02-05 17:25:54');
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,`created`) VALUES (6,'Paul','paul@superpochta.ru','M','1998-08-12','123789',1,'2026-02-05 17:26:22');
